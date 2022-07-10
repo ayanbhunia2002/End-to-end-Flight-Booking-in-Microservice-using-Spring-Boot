7967cc2d31eedda2f8ad7f55f80be08c2bf5b4d2
ayanbhunia2002 <94762854+ayanbhunia2002@users.noreply.github.com>
2022-07-10 17:10:29 +0530

    Delete Spring Boot/Passenger_DAL directory

--- Spring Boot/Passenger_DAL/src/main/java/com/ayan/passenger/dal/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ayan.passenger.dal.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ayan.passenger.dal.entities.Passenger;
-
-public interface PassengerRepository extends CrudRepository<Passenger, Long> {
-
-}
